99f200bbd3220f2b4dd2338d55679a2df3ad63b4
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-08 03:45:13 -0300

    Creación de Interfaz IEntitySeeder.cs

---
+++ ApiTechOil/DataAccess/DataBaseSeeding/IEntitySeeder.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TechOilAPI.DataAccess.DataBaseSeeding
+{
+    public interface IEntitySeeder
+    {
+        void SeedDataBase(ModelBuilder modelBuilder);
+    }
+}
