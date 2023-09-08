446acf86534865ece13d1a91604951f9f32dc361
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-08 03:24:08 -0300

    Creación Interfaz IUsuarioRepository.cs

---
+++ ApiTechOil/Repository/IUsuarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using TechOilAPI.Entities;
+
+namespace TechOilAPI.Repository
+{
+    public interface IUsuarioRepository{
+         IEnumerable<Usuario>GetAllUsuarios();
+         Usuario GetUsuarioById(int id);
+         void AddUsuario(Usuario usuario);
+         void UpdateUsuario(Usuario usuario);
+         void DeleteUsuario(int id);
+    }
+}
