d8d6878190ac02e1b06eef9e3d04746ba75a961b
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-08 03:00:17 -0300

    Creacion Entity Usuario

---
+++ ApiTechOil/Entities/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechOilAPI.Entities
+{
+    public class Usuario
+    {
+        [Key]
+        [Column("CodUsuario", TypeName = "int")]
+        public int CodUsuario { get; set; }
+        [Required]
+        [Column("Nombre", TypeName = "VARCHAR (100)")]
+        public string Nombre { get; set; }  
+        [Required]
+        [Column("Dni", TypeName = "int")]
+        public int Dni { get; set; }
+        [Required]
+        [Column("PerfilUsuario", TypeName = "int")]
+        public int PerfilUsuario { get; set; }
+        [Required]
+        [Column("CLave", TypeName = "VARCHAR (100)")]
+        public string Clave { get; set; }
+
+    }
+}
