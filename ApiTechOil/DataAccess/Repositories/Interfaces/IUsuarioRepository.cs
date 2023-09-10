fb8940e8a282f0957eea32e2ae2ed364a9e9dfdf
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-10 01:20:24 -0300

    Implementación repositories

---
+++ ApiTechOil/DataAccess/Repositories/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApiTechOil.Entities;
+
+namespace ApiTechOil.DataAccess.Repositories.Interfaces
+{
+    public interface IUsuarioRepository :IRepository<Usuario>
+    {
+        //IEnumerable<Usuario> GetAllUsuarios();
+        //Usuario GetUsuarioById(int id);
+        //void AddUsuario(Usuario usuario);
+        //void UpdateUsuario(Usuario usuario);
+        //void DeleteUsuario(int id);
+    }
+}
