09831044d2f268a9e60b60ae5643f2faf824febd
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-12 12:02:27 -0300

    Creación RegisterDto.cs

---
+++ ApiTechOil/DTOs/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiTechOil.DTOs
+{
+    public class RegisterDto
+    {
+        public string Nombre { get; set; }
+        public int Dni { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+    }
+}
