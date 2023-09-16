b9c288c4e18c08c390c2eb5379f88f84c06bcc27
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-15 23:18:42 -0300

    Actualización Unit of Work

--- ApiTechOil/Services/IUnitOfWork.cs
+++ ApiTechOil/Services/IUnitOfWork.cs
@@ -1,9 +1,13 @@
 ﻿using ApiTechOil.DataAccess.Repositories;
+using ApiTechOil.DataAccess.Repositories.Interfaces;
 
 namespace ApiTechOil.Services
 {
     public interface IUnitOfWork
     {
+        public ProyectosRepository ProyectosRepository { get; }
+        public ServiciosRepository ServiciosRepository { get; }
+        public TrabajosRepository TrabajosRepository { get; }
         public UsuarioRepository UsuarioRepository { get; }
         Task<int> Complete();
     }
