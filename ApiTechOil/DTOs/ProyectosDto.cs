edd5d10698e38e2b731011de046e3a3177688c32
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-15 23:17:42 -0300

    Creación DTOs

---
+++ ApiTechOil/DTOs/ProyectosDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiTechOil.DTOs
+{
+    public class ProyectosDto
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public int Estado { get; set; }
+
+    }
+}
---
+++ ApiTechOil/DTOs/ServiciosDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiTechOil.DTOs
+{
+    public class ServiciosDto
+    {
+        public string Descr { get; set; }
+        public bool Estado { get; set; }
+        public double ValorHora { get; set; }
+
+    }
+}
