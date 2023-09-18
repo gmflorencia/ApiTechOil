47ef54e745a989d2a607c1888f96265fa5ffee62
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-17 21:36:47 -0300

    Creación Controller y Repository de PerfilUsuario

---
+++ ApiTechOil/Controllers/PerfilUsuarioController.cs
@@ -0,0 +1,80 @@
+﻿using ApiTechOil.DTOs;
+using ApiTechOil.Entities;
+using ApiTechOil.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace ApiTechOil.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerfilUsuarioController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public PerfilUsuarioController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PerfilUsuario>>> GetAll()
+        {
+            var PerfilUsuario = await _unitOfWork.PerfilUsuarioRepository.GetAll();
+
+            return PerfilUsuario;
+        }
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<PerfilUsuario>> GetById(int id)
+        {
+            var perfilUsuario = await _unitOfWork.PerfilUsuarioRepository.GetById(id);
+            if (perfilUsuario == null)
+            {
+                return NotFound();
+            }
+            return perfilUsuario;
+        }
+
+        [Authorize(Policy = "Administrador")]
+        [HttpPost]
+        [Route("PerfilUsuario")]
+        public async Task<IActionResult> Insert(PerfilUsuarioDto dto)
+        {
+
+            var perfilUsuario = new PerfilUsuario(dto);
+            await _unitOfWork.PerfilUsuarioRepository.Insert(perfilUsuario);
+            await _unitOfWork.Complete();
+            return Ok(true);
+
+        }
+
+        [Authorize(Policy = "Administrador")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, PerfilUsuarioDto dto)
+        {
+            var result = await _unitOfWork.PerfilUsuarioRepository.Update(new PerfilUsuario(dto, id));
+
+            await _unitOfWork.Complete();
+            return Ok(true);
+        }
+
+        [Authorize(Policy = "Administrador")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            PerfilUsuario perfilUsuario = await _unitOfWork.PerfilUsuarioRepository.GetById(id);
+            if (perfilUsuario == null)
+            {
+                return NotFound(); // Devuelve un resultado NotFound si el usuario no se encuentra.
+            }
+            var result = await _unitOfWork.PerfilUsuarioRepository.Update(new PerfilUsuario
+            {
+                Id = id,
+                Descripcion = perfilUsuario.Descripcion,
+                Activo = false
+            });
+            if (result) await _unitOfWork.Complete();
+            return Ok(result);
+        }
+    }
+}
