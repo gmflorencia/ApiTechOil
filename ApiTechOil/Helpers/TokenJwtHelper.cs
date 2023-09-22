b0f1b8ca86cff0c28a1ace0a40600341e6c976fc
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-09-22 01:51:37 -0300

    Refactor Helpers

--- ApiTechOil/Helpers/TokenJwtHelper.cs
+++ ApiTechOil/Helpers/TokenJwtHelper.cs
@@ -26,7 +26,7 @@ namespace ApiTechOil.Helpers
                 new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
                 new Claim(ClaimTypes.NameIdentifier, usuario.CodUsuario.ToString()),
                 new Claim(ClaimTypes.Email, usuario.Email),
-                new Claim(ClaimTypes.Role, usuario.PerfilUsuario.ToString()),
+                new Claim(ClaimTypes.Role, usuario.CodRol.ToString()),
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
